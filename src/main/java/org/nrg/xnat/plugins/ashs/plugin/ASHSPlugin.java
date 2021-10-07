1edb085ecb5641dab4457a631416c5d10aae7445
stimon <santytimon@gmail.com>
2021-10-07 10:12:36 +0200

    Version 1.0.0

--- src/main/java/org/nrg/xnat/plugins/ashs/plugin/ASHSPlugin.java
+++ src/main/java/org/nrg/xnat/plugins/ashs/plugin/ASHSPlugin.java
@@ -7,20 +7,20 @@
  * Released under the Simplified BSD.
  */
 
-package org.nrg.xnat.plugins.template.plugin;
+package org.nrg.xnat.plugins.ashs.plugin;
 
 import lombok.extern.slf4j.Slf4j;
 import org.nrg.config.services.ConfigService;
 import org.nrg.framework.annotations.XnatDataModel;
 import org.nrg.framework.annotations.XnatPlugin;
-import org.nrg.xdat.bean.AshsAshsdataBean;
+// import org.nrg.xdat.bean.AshsAshsdataBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 
 @XnatPlugin(value = "ASHS-Plugin", name = "XNAT ASHS Plugin", description="Provides the functionality to store ASHS results as image assessors.",
-            dataModels = {@XnatDataModel(value = AshsAshsdataBean.SCHEMA_ELEMENT_NAME,
+            dataModels = {@XnatDataModel(value = "ashs:ashsData",//AshsAshsdataBean.SCHEMA_ELEMENT_NAME,
                                          singular = "ASHS",
-                                         plural = "ASHSs",
+                                         plural = "ASHS",
                                          code = "ASHS")})
 
 @Slf4j
